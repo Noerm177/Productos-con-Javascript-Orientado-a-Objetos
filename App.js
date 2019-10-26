988867693024c0e98241a215e72dc2b6bf071060
noerm <noerm177@gmail.com>
2019-10-25 20:21:48 -0500

    mostrar podructo en pantalla

--- App.js
+++ App.js
@@ -1,5 +1,49 @@
 class Product {
-    constructor(){
-        
+    constructor(name, price, year) {
+        this.name = name;
+        this.price = price;
+        this.year = year;
     }
 }
+
+class UI {
+    addProduct(product) {
+        const productList = document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="card text-center mb-4">
+                <div class="card-body">
+                    <strong>Product name</strong>: ${product.name}
+                    <strong>Product price</strong>: ${product.price}
+                    <strong>Product year</strong>: ${product.year}
+                </div>
+            </div>
+       `;
+        productList.appendChild(element);
+
+    }
+
+    delProduct() {
+
+    }
+
+    showMessage() {
+
+    }
+}
+
+//Dom Events
+
+document.getElementById('product-form')
+    .addEventListener("submit", function (e) {
+        const name = document.getElementById('name').value;
+        const price = document.getElementById('price').value;
+        const year = document.getElementById('year').value;
+
+        const product = new Product(name, price, year);
+
+        const ui = new UI();
+        ui.addProduct(product);
+
+        e.preventDefault();
+    })
